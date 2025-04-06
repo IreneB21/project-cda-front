df84ec6fbc318bd6c7ebdde33866e5ca467355f8
IreneB21 <irenebouslama@gmail.com>
2025-04-06 10:20:22 +0200

    correction des lien de navigation dans la landing page

--- src/app/components/shared/landing/landing.component.ts
+++ src/app/components/shared/landing/landing.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [HeaderComponent, RouterOutlet, ButtonComponent],
+  imports: [HeaderComponent, ButtonComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
